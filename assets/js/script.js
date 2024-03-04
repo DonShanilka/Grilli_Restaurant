1bb8b06a06b6848b158dcbb2ca8f435110c5cc24
Shanilka <shanilka799@gmail.com>
2024-03-04 13:25:45 +0530

    loading js and add html top bar

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// preload
+// loading will be end after document is loaded
+const preloader = document.querySelector("[data-preaload]");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
