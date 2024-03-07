e08a8cba14d12f3e0232e1045edb95bfee269a5d
Shanilka <shanilka799@gmail.com>
2024-03-07 11:37:44 +0530

    update header js

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,6 +36,19 @@ addEventOnElement(navbarTogglers, "click", togglerNavbar);
 const header = document.querySelector("[data-header]");
 const backTopBtn = document.querySelector("[data-back-top-btn]");
 
+let lastScrollPos = 0;
+
+const hideHeader = function(){
+  const isScrollBottom = lastScrollPos < window.scrollY;
+  if (isScrollBottom) {
+    header.classList.add("hide");
+  } else {
+    header.classList.remove("hide");
+  }
+
+  lastScrollPos = window.screenY;
+}
+
 
 
 
